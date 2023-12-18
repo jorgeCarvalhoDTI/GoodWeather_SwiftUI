fc630a9d0c39cb4414c6220a6698a5886dbbd236
Jorge de Carvalho <jorgercj@dcomp.ufs.br>
2023-12-18 11:24:47 -0300

    Initial Commit

---
+++ GoodWeather_SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GoodWeather_SwiftUI
+//
+//  Created by Jorge de Carvalho on 18/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GoodWeather_SwiftUI/GoodWeather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodWeather_SwiftUIApp.swift
+//  GoodWeather_SwiftUI
+//
+//  Created by Jorge de Carvalho on 18/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GoodWeather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
